b91d7ecaf06b64f9074f3665692aff733b7ef25a
AndreAmaduzzi <amaduzziandrea@gmail.com>
2022-10-03 21:47:00 +0200

    add utils functions

---
+++ utils/misc.py
@@ -0,0 +1,22 @@
+import logging
+logger = logging.getLogger()
+import os
+import sys
+
+
+def setup_logging(output_dir):
+    log_format = logging.Formatter("%(asctime)s : %(message)s")
+    logger = logging.getLogger()
+    logger.handlers = []
+    output_file = os.path.join(output_dir, 'output.log')
+    file_handler = logging.FileHandler(output_file)
+    file_handler.setFormatter(log_format)
+    logger.addHandler(file_handler)
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(log_format)
+    err_handler = logging.StreamHandler(sys.stderr)
+    err_handler.setFormatter(log_format)
+    logger.addHandler(err_handler)
+    logger.setLevel(logging.INFO)
+
+    return logger
