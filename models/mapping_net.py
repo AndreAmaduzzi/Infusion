32bb579146a8c910b5cea340fe46f93bf2b30321
AndreAmaduzzi <amaduzziandrea@gmail.com>
2022-10-04 19:13:00 +0200

    implement Gaussian mapping of predicted noise

--- models/mapping_net.py
+++ models/mapping_net.py
@@ -2,6 +2,9 @@ import math
 import torch
 from torch import nn, Tensor
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
+import torch.distributions as distr
+
+from models.pvd import GaussianDiffusion
 
 '''params'''
 ''''
@@ -27,7 +30,19 @@ class MappingNet(nn.Module):
         if self.cls_token is not None:
             nn.init.normal_(self.cls_token, std=1e-6) # from rwightman code
         self.proj.bias.data.zero_()
-        self.proj.weight.data.uniform_(-initrange, initrange)  
+        self.proj.weight.data.uniform_(-initrange, initrange)
+    
+    def map_gaussian(self, input_pts: Tensor,
+                    eps: float = 10e-6) -> Tensor:
+                
+        input_pts = (input_pts - torch.min(input_pts, dim=1, keepdim=True)[0]) / (torch.max(input_pts, dim=1, keepdim=True)[0] - torch.min(input_pts, dim=1, keepdim=True)[0])
+        input_pts = torch.minimum(input_pts, input_pts - eps)
+        input_pts = torch.maximum(input_pts, torch.FloatTensor([eps]).cuda())
+
+        normal = distr.Normal(loc=torch.tensor([0.0]).cuda(), scale=torch.tensor([1.0]).cuda())
+        gaussian_pts = normal.icdf(value=input_pts)
+
+        return gaussian_pts
 
     def forward(self, x: Tensor, src_key_padding_mask: Tensor) -> Tensor:
         """
@@ -36,10 +51,11 @@ class MappingNet(nn.Module):
         Returns:
             output Tensor of shape [seq_len, batch_size, embed_size]
         """
-        src = torch.cat((self.cls_token.expand(-1, x.shape[1], -1), x), dim=0)  # x         [seq_length, batch_size, embed_size] =  [16, batch_size, 1024] 
-        src = self.pos_encoder(src)                                             # src       [seq_length+1, batch_size, 1024]     =  [17, batch_size, 1024] 
+        src = torch.cat((self.cls_token.expand(-1, x.shape[1], -1), x), dim=0)  # x         [seq_length, batch_size, embed_size] =  [60, batch_size, 1024] 
+        src = self.pos_encoder(src)                                             # src       [seq_length+1, batch_size, 1024]     =  [61, batch_size, 1024] 
         src = self.transformer_encoder(src, src_key_padding_mask=src_key_padding_mask)                           # src_mask  [batch_size, seq_length]
         output = self.proj(src[0,:])
+        output = self.map_gaussian(output)
         output = output.reshape(output.shape[0], 3, -1)
         return output                                                           # output [batch_size, 6144] => [batch_size, 2048, 3]
 
