cee4e75047526d2b153d6bdc9d73a051699a005d
AndreAmaduzzi <amaduzziandrea@gmail.com>
2022-10-11 22:31:33 +0200

    implement cloud visualization

--- utils/misc.py
+++ utils/misc.py
@@ -3,6 +3,8 @@ logger = logging.getLogger()
 import os
 import sys
 import torch
+import matplotlib.pyplot as plt
+import numpy as np
 
 def setup_logging(output_dir):
     log_format = logging.Formatter("%(asctime)s : %(message)s")
@@ -28,3 +30,30 @@ def normalize_cloud(pcd: torch.Tensor):
     new_pcd = (pcd - bc) / torch.unsqueeze(max_dist, dim=2)
 
     return new_pcd
+
+def visualize_pointcloud_batch(path, pointclouds, pred_labels, labels, categories, vis_label=False, target=None,  elev=30, azim=225):
+    batch_size = len(pointclouds)
+    fig = plt.figure(figsize=(20,20))
+
+    ncols = int(np.sqrt(batch_size))
+    nrows = max(1, (batch_size-1) // ncols+1)
+    for idx, pc in enumerate(pointclouds):
+        if vis_label:
+            label = categories[labels[idx].item()]
+            pred = categories[pred_labels[idx]]
+            colour = 'g' if label == pred else 'r'
+        elif target is None:
+
+            colour = 'g'
+        else:
+            colour = target[idx]
+        pc = pc.cpu().numpy()
+        ax = fig.add_subplot(nrows, ncols, idx + 1, projection='3d')
+        ax.scatter(pc[:, 0], pc[:, 2], pc[:, 1], c=colour, s=5)
+        ax.view_init(elev=elev, azim=azim)
+        ax.axis('off')
+        if vis_label:
+            ax.set_title('GT: {0}\nPred: {1}'.format(label, pred))
+
+    plt.savefig(path)
+    plt.close(fig)
